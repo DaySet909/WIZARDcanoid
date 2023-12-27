1b6c598c73b9062792f7e193ac7502070a8e8fc7
DaySet909 <78254936+DaySet909@users.noreply.github.com>
2023-12-28 02:12:16 +0300

    На пути к цвету и форме
    - отредактирован цикл для вывода кирпичей в форму

--- Assets/Scripts/BricksMeneg.cs
+++ Assets/Scripts/BricksMeneg.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 
 public class BricksMeneg : MonoBehaviour
 {
-    public GameObject [] bricksArray;
+    public GameObject[] bricksArray;
     public GameObject brick;
     private SpriteRenderer spriteRenderer;
     public int countOfBrickX = 3;
     public int countOfBrickY = 3;
+    int[,] borick = new int[7, 58] 
+    
+    {   { 1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0},
+        { 0,0,0,0,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,1,1,0,0,0,1,1,0,1,1,0,0,0},
+        { 0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,1,1,0,0},
+        { 0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,1,1,0},
+        { 0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0},
+        { 0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0},
+        { 1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0} };
+
+
     float scaleX;
     float scaleY;
 
@@ -19,12 +30,13 @@ public class BricksMeneg : MonoBehaviour
         scaleX = spriteRenderer.bounds.size.x;
         scaleY = spriteRenderer.bounds.size.y;
 
-        for (int i = 0; i < countOfBrickX; i++)
+        for (int y = 0; y < borick.GetLength(1); y++)
         {
-            for (int j = 0; j < countOfBrickY; j++)
+            for (int x = 0; x < borick.GetLength(0); x++)
             {
-                var brickRandom = bricksArray[Random.Range(0, bricksArray.Length)];
-                Instantiate(brickRandom, transform.position + ( new Vector3(i * scaleX, j * -scaleY, 0)), Quaternion.identity);
+               // var brickRandom = bricksArray[Random.Range(0, bricksArray.Length)];
+                //int brickRandom = borick[i,j];
+                Instantiate(bricksArray[borick[x , y]], transform.position + ( new Vector3(y * scaleX, x * -scaleY, 0)), Quaternion.identity);
             }
         }
     }
